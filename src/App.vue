9bff2cab751686f8691122f03a94c2b5863c7a69
dimaldo86 <dimaldo86ne@mail.ru>
2023-06-17 03:08:41 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+	<button type="button" class="btn btn-primary">Primary</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Import our custom CSS
+
+import './scss/main.scss'
+
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
